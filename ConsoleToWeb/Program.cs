f6bbb54d79b92fd8973c94dae962fa74992ade99
dinnerhe <dinnerhe@hotmail.com>
2022-09-12 14:38:24 -0700

    Converted

--- ConsoleToWeb/Program.cs
+++ ConsoleToWeb/Program.cs
@@ -1,3 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
 
+namespace ConsoleToWeb {
+    class Program {
+        public static void Main(string[] args) {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseEndpoints(async endpoint =>
+            {
+                endpoint.MapGet("/", async context=> {
+                    await context.Response.WriteAsync("Hello World!by Tony");
+                });
+            });
+            app.Run();
+        }
+       
+
+    }
+
+}
